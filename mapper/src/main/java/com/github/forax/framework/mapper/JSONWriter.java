44a6c48b646852d00b4bf901174f85e78314212c
Mingotaud Gabriel <gabrielmingotaud@gmail.com>
2025-09-10 18:11:32 +0200

    fix JSONWriter

--- mapper/src/main/java/com/github/forax/framework/mapper/JSONWriter.java
+++ mapper/src/main/java/com/github/forax/framework/mapper/JSONWriter.java
@@ -1,8 +1,65 @@
+//package com.github.forax.framework.mapper;
+//
+//import jdk.jshell.execution.Util;
+//
+//import java.beans.Introspector;
+//import java.beans.PropertyDescriptor;
+//import java.lang.reflect.Method;
+//import java.util.Arrays;
+//import java.util.List;
+//import java.util.stream.Collectors;
+//
+//public final class JSONWriter {
+//
+//    private interface Generator {
+//        String generate(JSONWriter writer, Object bean);
+//    }
+//
+//    private record Property(String prefix, Method getter) {}
+//
+//    private static final class PropertyDescriptorClassValue
+//            extends ClassValue<List<Property>> {
+//        @Override
+//        protected List<Property> computeValue(Class<?> type) {
+//            var beanInfo = Utils.beanInfo(type);
+//            return Arrays.stream(beanInfo.getPropertyDescriptors())
+//                    .map(propertyDescriptor -> {
+//                        var name = propertyDescriptor.getName();
+//                        var getter = propertyDescriptor.getReadMethod();
+//                        var prefix = "\"" + name + "\": ";
+//                        return new Property(prefix, getter);
+//                    }).toList();
+//        }
+//    }
+//
+//    private static final PropertyDescriptorClassValue CLASS_VALUE = new PropertyDescriptorClassValue();
+//
+//    private String toJSONObject(Object o) {
+//        var propertyDescriptors = CLASS_VALUE.get(o.getClass());
+//        return propertyDescriptors.stream()
+//                .map(property -> {
+//                    var value = Utils.invokeMethod(o, property.getter());
+//                    return property.prefix() + toJSON(value);
+//                }).collect(Collectors.joining(", ", "{", "}"));
+//    }
+//
+//    public String toJSON(Object o) {
+//        return switch (o){
+//            case Boolean b -> "" + b;
+//            case Integer i -> "" + i;
+//            case String s -> '"' + s + "\"";
+//            case Double d -> "" + d;
+//            case null -> "null";
+//            case Object _ -> toJSONObject(o);
+//        };
+//    }
+//}
+
 package com.github.forax.framework.mapper;
 
-import jdk.jshell.execution.Util;
+import jdk.jfr.Percentage;
 
-import java.beans.Introspector;
+import java.beans.BeanInfo;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.Method;
 import java.util.Arrays;
@@ -10,47 +67,52 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public final class JSONWriter {
-
-    private interface Generator {
-        String generate(JSONWriter writer, Object bean);
+    private interface JSONFuntion {
+        String apply(JSONWriter writer, Object instance);
     }
 
-    private record Property(String prefix, Method getter) {}
+    //private record Property(String prefix, Method getter){}
 
-    private static final class PropertyDescriptorClassValue
-            extends ClassValue<List<Property>> {
+    private static final class PropertyDescriptorClassValue extends ClassValue<List<JSONFuntion>>{
         @Override
-        protected List<Property> computeValue(Class<?> type) {
+        protected List<JSONFuntion> computeValue(Class<?> type){
             var beanInfo = Utils.beanInfo(type);
             return Arrays.stream(beanInfo.getPropertyDescriptors())
-                    .map(propertyDescriptor -> {
+                    .filter(propertyDescriptor -> !propertyDescriptor.getName().equals("class"))
+                    .<JSONFuntion>map(propertyDescriptor -> {
                         var name = propertyDescriptor.getName();
                         var getter = propertyDescriptor.getReadMethod();
                         var prefix = "\"" + name + "\": ";
-                        return new Property(prefix, getter);
-                    }).toList();
+                        return (JSONWriter writer, Object o) -> {
+                            var value = Utils.invokeMethod(o,getter);
+                            return prefix + writer.toJSON(value);
+                        };
+                    })
+                    .toList();
         }
     }
-
     private static final PropertyDescriptorClassValue CLASS_VALUE = new PropertyDescriptorClassValue();
 
-    private String toJSONObject(Object o) {
+    private String toJSONObject(Object o){
         var propertyDescriptors = CLASS_VALUE.get(o.getClass());
+        //var beanInfo = Utils.beanInfo(o.getClass());
         return propertyDescriptors.stream()
-                .map(property -> {
-                    var value = Utils.invokeMethod(o, property.getter());
-                    return property.prefix() + toJSON(value);
-                }).collect(Collectors.joining(", ", "{", "}"));
+                .map(jsonFuntion -> jsonFuntion.apply(this, o))
+                .collect(Collectors.joining(", ","{","}"));
     }
 
-    public String toJSON(Object o) {
-        return switch (o){
+    public String toJSON(Object o){
+        return switch(o){
             case Boolean b -> "" + b;
             case Integer i -> "" + i;
             case String s -> '"' + s + "\"";
             case Double d -> "" + d;
             case null -> "null";
-            case Object _ -> toJSONObject(o);
+            case Object _ -> {
+                yield toJSONObject(o);
+            }
+
+            //no need if case Object qui prend tout : default -> throw new IllegalArgumentException("Unexpected value:" + o);
         };
     }
 }
